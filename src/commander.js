ba3cc5c8b9425d54a7171d61eba5f6a974108b38
asekdev <andy.sek94@gmail.com>
2019-05-28 15:09:39 +1000

    added version

--- src/commander.js
+++ src/commander.js
@@ -2,10 +2,12 @@
 const program = require("commander");
 const { getYeQuote } = require("../index");
 
+program.version("1.0.0");
+
 program
 	.command("quote")
 	.alias("q")
-	.description("Get a random Kanye quote.")
+	.description("Get a random Kanye quote")
 	.action(() => {
 		getYeQuote();
 	});
